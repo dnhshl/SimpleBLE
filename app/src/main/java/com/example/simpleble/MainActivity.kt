c9c2bb1e2b38995f74d37ca5851a65d1da6c6cf0
ba1999 <andrej.boldt@outllok>
2021-03-30 17:41:35 +0200

    Scan nach BLE Geräten

--- app/src/main/java/com/example/simpleble/MainActivity.kt
+++ app/src/main/java/com/example/simpleble/MainActivity.kt
@@ -1,17 +1,34 @@
 package com.example.simpleble
 
+import android.Manifest
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
 import android.widget.Toast
 import splitties.toast.toast
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = "MainActivity"
+
+    private val btnDiscoverDevices : Button by lazy{ findViewById(R.id.buttonDiscoverDevices) }
+    private val listview: ListView by lazy { findViewById(R.id.listview) }
+
     private lateinit var bluetoothAdapter: BluetoothAdapter
+    private lateinit var scanner: BluetoothLeScanner
+
+    private var isScanning = false
+
+    private var discoveredDevices = arrayListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +48,26 @@ class MainActivity : AppCompatActivity() {
             toast(getString(R.string.ble_not_supported))
             finish()
         }
+
+        scanner = bluetoothAdapter.bluetoothLeScanner
+
+        btnDiscoverDevices.setOnClickListener {
+
+            checkBTPermission()
+
+            if (!isScanning) { // Suche ist nicht gestartet
+                scanner.startScan(scanCallback)
+                //scanner?.startScan(scanCallback)
+                Log.i(TAG, "Starte Scan")
+                isScanning = true
+                btnDiscoverDevices.text = getString(R.string.stop_search_device)
+            } else {                        // Suche ist gestartet
+                scanner.stopScan(scanCallback)
+                Log.i(TAG, "Stoppe Scan")
+                isScanning = false
+                btnDiscoverDevices.text = getString(R.string.start_search_device)
+            }
+        }
     }
 
     override fun onResume() {
@@ -39,5 +76,41 @@ class MainActivity : AppCompatActivity() {
             val turnBTOn = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(turnBTOn, 1)
         }
+    }
+
+    private val scanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            val deviceInfo = """${result.device.name}${result.device.address}""".trimIndent()
+            Log.i(TAG, "DeviceFound: $deviceInfo")
+
+            // gefundenes Gerät der Liste hinzufügen, wenn es noch nicht aufgeführt ist
+            if (!discoveredDevices.contains(deviceInfo)) {
+                discoveredDevices.add(deviceInfo)
+            }
+
+            // aktualisierte Liste im Listview anzeigen
+            val adapt = ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1,
+                discoveredDevices)
+            listview.adapter = adapt
+        }
+    }
+
+    private fun checkBTPermission() {
+        var permissionCheck = checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION")
+        permissionCheck += checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION")
+        if (permissionCheck != 0) {
+            requestPermissions(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ), 1001
+            )
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Aufräumen
+        scanner.stopScan(scanCallback)
     }
 }
