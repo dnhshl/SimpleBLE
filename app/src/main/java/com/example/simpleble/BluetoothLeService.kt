bcdf77f6cad328da8b068ac3ad41e3a5d8b8c629
ba1999 <andrej.boldt@outllok>
2021-03-31 13:00:21 +0200

    BLE Steuerung über Service

---
+++ app/src/main/java/com/example/simpleble/BluetoothLeService.kt
@@ -0,0 +1,303 @@
+package com.example.simpleble
+
+import android.app.Service
+import android.bluetooth.*
+import android.content.Context
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+import android.util.Log
+import java.util.*
+
+
+class BluetoothLeService : Service() {
+
+    private val TAG = BluetoothLeService::class.java.simpleName
+
+    private val STATE_DISCONNECTED = 0
+    private val STATE_CONNECTING = 1
+    private val STATE_CONNECTED = 2
+
+    private var mBluetoothManager: BluetoothManager? = null
+    private var mBluetoothAdapter: BluetoothAdapter? = null
+    private var mBluetoothDeviceAddress: String? = null
+    private var mBluetoothGatt: BluetoothGatt? = null
+    private var mConnectionState = STATE_DISCONNECTED
+
+    companion object {
+        val ACTION_GATT_CONNECTED = "com.example.bluetooth.le.ACTION_GATT_CONNECTED"
+        val ACTION_GATT_DISCONNECTED = "com.example.bluetooth.le.ACTION_GATT_DISCONNECTED"
+        val ACTION_GATT_SERVICES_DISCOVERED = "com.example.bluetooth.le.ACTION_GATT_SERVICES_DISCOVERED"
+        val ACTION_DATA_AVAILABLE = "com.example.bluetooth.le.ACTION_DATA_AVAILABLE"
+        val EXTRA_DATA = "com.example.bluetooth.le.EXTRA_DATA"
+
+
+        val GATT_SERVICE_UUID = "0000ffe0-0000-1000-8000-00805f9b34fb"
+        val GATT_CHARACTERISTIC_UUID = "0000ffe1-0000-1000-8000-00805f9b34fb"
+        val CLIENT_CHARACTERISTIC_CONFIG_UUID = "00002902-0000-1000-8000-00805f9b34fb"
+    }
+
+    // Implements callback methods for GATT events that the app cares about.  For example,
+    // connection change and services discovered.
+    private val mGattCallback: BluetoothGattCallback = object : BluetoothGattCallback() {
+        override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+            val intentAction: String
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                intentAction = ACTION_GATT_CONNECTED
+                mConnectionState = STATE_CONNECTED
+                broadcastUpdate(intentAction)
+                Log.i(TAG, "Connected to GATT server.")
+                // Attempts to discover services after successful connection.
+                refreshDeviceCache(mBluetoothGatt!!, true)
+                Log.i(
+                        TAG, "Attempting to start service discovery:" +
+                        mBluetoothGatt!!.discoverServices()
+                )
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                intentAction = ACTION_GATT_DISCONNECTED
+                mConnectionState = STATE_DISCONNECTED
+                close()
+                Log.i(TAG, "Disconnected from GATT server.")
+                broadcastUpdate(intentAction)
+            }
+        }
+
+        override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                broadcastUpdate(ACTION_GATT_SERVICES_DISCOVERED)
+            } else {
+                Log.w(TAG, "onServicesDiscovered received: $status")
+            }
+        }
+
+        override fun onCharacteristicRead(
+                gatt: BluetoothGatt,
+                characteristic: BluetoothGattCharacteristic,
+                status: Int
+        ) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                broadcastUpdate(ACTION_DATA_AVAILABLE, characteristic)
+            }
+        }
+
+        override fun onCharacteristicChanged(
+                gatt: BluetoothGatt,
+                characteristic: BluetoothGattCharacteristic
+        ) {
+            broadcastUpdate(ACTION_DATA_AVAILABLE, characteristic)
+        }
+    }
+
+    private fun broadcastUpdate(action: String) {
+        val intent = Intent(action)
+        sendBroadcast(intent)
+    }
+
+    private fun broadcastUpdate(
+            action: String,
+            characteristic: BluetoothGattCharacteristic
+    ) {
+        val intent = Intent(action)
+        sendBroadcast(intent)
+    }
+
+    inner class LocalBinder : Binder() {
+        fun getService() : BluetoothLeService {
+            return this@BluetoothLeService
+        }
+
+    }
+
+
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return mBinder
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        // After using a given device, you should make sure that BluetoothGatt.close() is called
+        // such that resources are cleaned up properly.  In this particular example, close() is
+        // invoked when the UI is disconnected from the Service.
+        close()
+        return super.onUnbind(intent)
+    }
+
+    private val mBinder: IBinder = LocalBinder()
+
+    /**
+     * Initializes a reference to the local Bluetooth adapter.
+     *
+     * @return Return true if the initialization is successful.
+     */
+    fun initialize(): Boolean {
+        // For API level 18 and above, get a reference to BluetoothAdapter through
+        // BluetoothManager.
+        if (mBluetoothManager == null) {
+            mBluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+            if (mBluetoothManager == null) {
+                Log.e(TAG, "Unable to initialize BluetoothManager.")
+                return false
+            }
+        }
+        mBluetoothAdapter = mBluetoothManager!!.getAdapter()
+        if (mBluetoothAdapter == null) {
+            Log.e(TAG, "Unable to obtain a BluetoothAdapter.")
+            return false
+        }
+        return true
+    }
+
+    /**
+     * Connects to the GATT server hosted on the Bluetooth LE device.
+     *
+     * @param address The device address of the destination device.
+     *
+     * @return Return true if the connection is initiated successfully. The connection result
+     * is reported asynchronously through the
+     * `BluetoothGattCallback#onConnectionStateChange(android.bluetooth.BluetoothGatt, int, int)`
+     * callback.
+     */
+    fun connect(address: String?): Boolean {
+        if (mBluetoothAdapter == null || address == null) {
+            Log.w(TAG, "BluetoothAdapter not initialized or unspecified address.")
+            return false
+        }
+
+        // Previously connected device.  Try to reconnect.
+        if (mBluetoothDeviceAddress != null && address == mBluetoothDeviceAddress && mBluetoothGatt != null) {
+            Log.d(TAG, "Trying to use an existing mBluetoothGatt for connection.")
+            return if (mBluetoothGatt!!.connect()) {
+                mConnectionState = STATE_CONNECTING
+                true
+            } else {
+                false
+            }
+        }
+        val device = mBluetoothAdapter!!.getRemoteDevice(address)
+        if (device == null) {
+            Log.w(TAG, "Device not found.  Unable to connect.")
+            return false
+        }
+        // We want to directly connect to the device, so we are setting the autoConnect
+        // parameter to false.
+        mBluetoothGatt = device.connectGatt(this, false, mGattCallback, BluetoothDevice.TRANSPORT_LE)
+        Log.d(TAG, "Trying to create a new connection.")
+        refreshDeviceCache(mBluetoothGatt!!, false)
+        mBluetoothDeviceAddress = address
+        mConnectionState = STATE_CONNECTING
+        return true
+    }
+
+    /**
+     * Disconnects an existing connection or cancel a pending connection. The disconnection result
+     * is reported asynchronously through the
+     * `BluetoothGattCallback#onConnectionStateChange(android.bluetooth.BluetoothGatt, int, int)`
+     * callback.
+     */
+    fun disconnect() {
+        if (mBluetoothAdapter == null || mBluetoothGatt == null) {
+            Log.w(TAG, "BluetoothAdapter not initialized")
+            return
+        }
+        mBluetoothGatt!!.disconnect()
+    }
+
+    /**
+     * After using a given BLE device, the app must call this method to ensure resources are
+     * released properly.
+     */
+    fun close() {
+        if (mBluetoothGatt == null) {
+            return
+        }
+        mBluetoothGatt!!.close()
+        mBluetoothGatt = null
+    }
+
+    /**
+     * Request a read on a given `BluetoothGattCharacteristic`. The read result is reported
+     * asynchronously through the `BluetoothGattCallback#onCharacteristicRead(android.bluetooth.BluetoothGatt, android.bluetooth.BluetoothGattCharacteristic, int)`
+     * callback.
+     *
+     * @param characteristic The characteristic to read from.
+     */
+    fun readCharacteristic(characteristic: BluetoothGattCharacteristic?) {
+        if (mBluetoothAdapter == null || mBluetoothGatt == null) {
+            Log.w(TAG, "BluetoothAdapter not initialized")
+            return
+        }
+        mBluetoothGatt!!.readCharacteristic(characteristic)
+    }
+
+    /**
+     * Enables or disables notification on a give characteristic.
+     *
+     * @param characteristic Characteristic to act on.
+     * @param enabled If true, enable notification.  False otherwise.
+     */
+    fun setCharacteristicNotification(
+            characteristic: BluetoothGattCharacteristic,
+            enabled: Boolean
+    ) {
+        if (mBluetoothAdapter == null || mBluetoothGatt == null) {
+            Log.w(TAG, "BluetoothAdapter not initialized")
+            return
+        }
+        mBluetoothGatt!!.setCharacteristicNotification(characteristic, enabled)
+        if (UUID.fromString(GATT_CHARACTERISTIC_UUID).equals(characteristic.uuid)) {
+            val descriptor = characteristic.getDescriptor(
+                    UUID.fromString(CLIENT_CHARACTERISTIC_CONFIG_UUID)
+            )
+            descriptor.value = BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE
+            mBluetoothGatt!!.writeDescriptor(descriptor)
+        }
+    }
+
+    fun writeCharacteristic(characteristic: BluetoothGattCharacteristic?) {
+        mBluetoothGatt!!.writeCharacteristic(characteristic)
+    }
+
+    /**
+     * Retrieves a list of supported GATT services on the connected device. This should be
+     * invoked only after `BluetoothGatt#discoverServices()` completes successfully.
+     *
+     * @return A `List` of supported services.
+     */
+    fun getSupportedGattServices(): List<BluetoothGattService?>? {
+        return if (mBluetoothGatt == null) null else mBluetoothGatt!!.getServices()
+    }
+
+    // Diese Funktion ist wichtig, wenn beim ESP32 eigendefinierte Services verwendet werden.
+    // Ändern sich diese, wurde das gleiche Gerät aber vorher schon einmal mit anderen Services genutzt,
+    // dann sind die alten Services noch gecached und die neuen werden bei einem discover nicht gefunden!!!
+    // https://devzone.nordicsemi.com/nordic/nordic-blog/b/blog/posts/what-to-keep-in-mind-when-developing-your-ble-andr
+    // https://devzone.nordicsemi.com/cfs-file/__key/communityserver-blogs-components-weblogfiles/00-00-00-00-04-DZ-1046/2604.BLE_5F00_on_5F00_Android_5F00_v1.0.1.pdf
+    protected fun refreshDeviceCache(gatt: BluetoothGatt, force: Boolean) {
+        /*
+         * If the device is bonded this is up to the Service Changed characteristic to notify Android
+         * that the services has changed. There is no need for this trick in that case.
+         * If not bonded, the Android should not keep the services cached when the Service Changed
+         * characteristic is present in the target device database.
+         * However, due to the Android bug, it is keeping them anyway and the only way to clear
+         * services is by using this hidden refresh method.
+         */
+        if (force || gatt.device.bondState == BluetoothDevice.BOND_NONE) {
+            //sendLogBroadcast(LOG_LEVEL_DEBUG, "gatt.refresh() (hidden)");
+            /*
+             * There is a refresh() method in BluetoothGatt class but for now it's hidden.
+             * We will call it using reflections.
+             */
+            try {
+                val refresh = gatt.javaClass.getMethod("refresh")
+                val success = refresh.invoke(gatt) as Boolean
+                Log.i(TAG, "Refreshing result: $success")
+            } catch (e: Exception) {
+                Log.e(TAG, "An exception occurred while refreshing device", e)
+                //sendLogBroadcast(LOG_LEVEL_WARNING, "Refreshing failed");
+            }
+        }
+    }
+
+}
+
+
